f9678428261d5456162544a21d40d939f802da72
Alumnos <Alumnos@SCL100PCLC30027.alumnos.cst>
2023-08-22 21:53:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/VerCartelera.java
@@ -0,0 +1,14 @@
+package com.example.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VerCartelera extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
